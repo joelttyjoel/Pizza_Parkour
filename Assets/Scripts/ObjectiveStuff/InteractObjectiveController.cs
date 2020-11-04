fdb4c13364b048697f2077f37a50d36de89a7be2
Joel Håkansson <joelttyjoel@hotmail.com>
2020-11-04 19:33:10 +0100

    Made working pickup and release system

--- Assets/Scripts/ObjectiveStuff/InteractObjectiveController.cs
+++ Assets/Scripts/ObjectiveStuff/InteractObjectiveController.cs
@@ -9,28 +9,36 @@ public class InteractObjectiveController : MonoBehaviour
     public GameObject pickUpHint;
     [Header("Settings")]
     public float pickupRange;
-    public Vector3 firstBoxPosition;
-    public Vector3 boxOffset;
+    public Transform firstBoxPosition;
+    public Vector3 objectiveOffset;
+    public GameObject[] allFakeColliders;
 
+    private Transform objectiveContainer = null;
     private Transform closestObjective = null;
     private float closestObjectiveDistance = 99999999;
-    private List<Transform> allObjectivesInScene = new List<Transform>();
+    private List<Transform> allAvailableObjectivesInScene = new List<Transform>();
     
+    private Stack<Transform> allObjectivesOnHead = new Stack<Transform>();
+
     void Start()
     {
         GameObject[] allObjectives = GameObject.FindGameObjectsWithTag(objectiveTag);
 
         for (int i = 0; i < allObjectives.Length; i++)
         {
-            allObjectivesInScene.Add(allObjectives[i].transform);
+            allAvailableObjectivesInScene.Add(allObjectives[i].transform);
         }
+
+        objectiveContainer = GameObject.Find("ObjectiveContainer").transform;
     }
     
     void Update()
     {
+        UpdateFakeColliderOnHead();
+
         //BURST can be added here mm yes but not needed because probably not more than like 20 boxes
         //find closest objective
-        foreach(Transform a in allObjectivesInScene)
+        foreach (Transform a in allAvailableObjectivesInScene)
         {
             float currentDistance = Vector3.Distance(transform.position, a.position);
             if (currentDistance < closestObjectiveDistance) closestObjective = a;
@@ -55,10 +63,56 @@ public class InteractObjectiveController : MonoBehaviour
     public void PickUpClosestObjective()
     {
         Debug.Log("pick up");
+
+        closestObjective.GetComponent<Rigidbody2D>().simulated = false;
+
+        //remove from available list, doing this before so only need to compare transforms and not get instance id
+        for (int i = 0; i < allAvailableObjectivesInScene.Count; i++)
+        {
+            if (allAvailableObjectivesInScene[i] == closestObjective)
+            {
+                allAvailableObjectivesInScene.RemoveAt(i);
+                break;
+            }
+        }
+        //reset closest distance so will check for ntext closest
+        closestObjectiveDistance = 9999999f;
+        //change transform and move
+        closestObjective.SetParent(firstBoxPosition);
+        closestObjective.position = firstBoxPosition.position + (objectiveOffset * allObjectivesOnHead.Count);
+        //add to stack on head
+        allObjectivesOnHead.Push(closestObjective);
     }
 
     public void DropTopObjective()
     {
+        if (allObjectivesOnHead.Count <= 0) return;
+
         Debug.Log("drop");
+
+        //remove from all on head
+        Transform objectToDrop = allObjectivesOnHead.Pop();
+
+        objectToDrop.GetComponent<Rigidbody2D>().simulated = true;
+        
+        //now that removed from head, remove top of fake ones, mm yes
+        UpdateFakeColliderOnHead();
+
+        //add back to list of all avaiable for pickup
+        allAvailableObjectivesInScene.Add(objectToDrop);
+
+        //change transform
+        objectToDrop.SetParent(objectiveContainer);
+        //find where to place, hardest part hmmm
+        
+    }
+
+    public void UpdateFakeColliderOnHead()
+    {
+        for(int i = 0; i < allFakeColliders.Length; i++)
+        {
+            if (i < allObjectivesOnHead.Count) allFakeColliders[i].SetActive(true);
+            else allFakeColliders[i].SetActive(false);
+        }
     }
 }
