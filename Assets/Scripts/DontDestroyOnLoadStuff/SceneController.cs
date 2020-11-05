2c397fd96171a85a76911d4c916fb1584cc19ba7
Joel Håkansson <joelttyjoel@hotmail.com>
2020-11-05 11:34:51 +0100

    Added scene switcher, working on adding stinky animated UI for cool epic game feel

---
+++ Assets/Scripts/DontDestroyOnLoadStuff/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public Object mainMenu;
+    public Object[] LevelsInOrderAscending;
+
+    public static SceneController Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    public void SwitchToLevelByIndex(int index)
+    {
+        SceneManager.LoadScene(LevelsInOrderAscending[index].name.ToString());
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenu.name);
+    }
+}
---
+++ Assets/UnuGames/UIManExample/Scripts/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnuGames;
+
+public class StartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		UIMan.Instance.ShowScreen<UIMainMenu> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
