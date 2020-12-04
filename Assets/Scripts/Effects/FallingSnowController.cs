0bdc2318cc447473a8e053a0554357ad548ce486
Joel <joelttyjoel@hotmail.com>
2020-12-04 20:46:15 +0100

    alot of shit apparently, i forget to commit

---
+++ Assets/Scripts/Effects/FallingSnowController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSnowController : MonoBehaviour
+{
+    public float timeForSnowToSpeedAtStart = 0.12f;
+    public List<ParticleSystem> snowSystems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SnowStartSuperFast());
+    }
+
+    private IEnumerator SnowStartSuperFast()
+    {
+        foreach (ParticleSystem a in snowSystems)
+        {
+            var main = a.main;
+            main.simulationSpeed = 999999999;
+        }
+        yield return new WaitForSecondsRealtime(timeForSnowToSpeedAtStart);
+        foreach (ParticleSystem a in snowSystems)
+        {
+            var main = a.main;
+            main.simulationSpeed = 1;
+        }
+    }
+}
